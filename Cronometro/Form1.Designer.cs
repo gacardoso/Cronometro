9b45f688036e64da655077551735a9a55647788b
“gacardoso” <gcardosoti@gmail.com>
2017-04-23 16:01:47 -0300

    src

---
+++ Cronometro/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Cronometro
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.lblTempo = new System.Windows.Forms.Label();
+			this.btnIniciar = new System.Windows.Forms.Button();
+			this.btnParcial = new System.Windows.Forms.Button();
+			this.btnParar = new System.Windows.Forms.Button();
+			this.tbParcial = new System.Windows.Forms.TextBox();
+			this.tmrCronometro = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// lblTempo
+			// 
+			this.lblTempo.AutoSize = true;
+			this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTempo.Location = new System.Drawing.Point(22, 49);
+			this.lblTempo.Name = "lblTempo";
+			this.lblTempo.Size = new System.Drawing.Size(216, 42);
+			this.lblTempo.TabIndex = 0;
+			this.lblTempo.Text = "00:00:00:00";
+			this.lblTempo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnIniciar
+			// 
+			this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnIniciar.Location = new System.Drawing.Point(12, 138);
+			this.btnIniciar.Name = "btnIniciar";
+			this.btnIniciar.Size = new System.Drawing.Size(242, 58);
+			this.btnIniciar.TabIndex = 1;
+			this.btnIniciar.Text = "Iniciar";
+			this.btnIniciar.UseVisualStyleBackColor = true;
+			this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+			// 
+			// btnParcial
+			// 
+			this.btnParcial.Enabled = false;
+			this.btnParcial.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnParcial.Location = new System.Drawing.Point(10, 202);
+			this.btnParcial.Name = "btnParcial";
+			this.btnParcial.Size = new System.Drawing.Size(119, 58);
+			this.btnParcial.TabIndex = 2;
+			this.btnParcial.Text = "Parcial";
+			this.btnParcial.UseVisualStyleBackColor = true;
+			this.btnParcial.Click += new System.EventHandler(this.btnParcial_Click);
+			// 
+			// btnParar
+			// 
+			this.btnParar.Enabled = false;
+			this.btnParar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnParar.Location = new System.Drawing.Point(135, 202);
+			this.btnParar.Name = "btnParar";
+			this.btnParar.Size = new System.Drawing.Size(119, 58);
+			this.btnParar.TabIndex = 3;
+			this.btnParar.Text = "Parar";
+			this.btnParar.UseVisualStyleBackColor = true;
+			this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+			// 
+			// tbParcial
+			// 
+			this.tbParcial.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tbParcial.Location = new System.Drawing.Point(260, 12);
+			this.tbParcial.Multiline = true;
+			this.tbParcial.Name = "tbParcial";
+			this.tbParcial.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbParcial.Size = new System.Drawing.Size(222, 248);
+			this.tbParcial.TabIndex = 4;
+			// 
+			// tmrCronometro
+			// 
+			this.tmrCronometro.Enabled = true;
+			this.tmrCronometro.Interval = 1;
+			this.tmrCronometro.Tick += new System.EventHandler(this.tmrCronometro_Tick);
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.btnIniciar;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(494, 272);
+			this.Controls.Add(this.tbParcial);
+			this.Controls.Add(this.btnParar);
+			this.Controls.Add(this.btnParcial);
+			this.Controls.Add(this.btnIniciar);
+			this.Controls.Add(this.lblTempo);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Cronômetro";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblTempo;
+		private System.Windows.Forms.Button btnIniciar;
+		private System.Windows.Forms.Button btnParcial;
+		private System.Windows.Forms.Button btnParar;
+		private System.Windows.Forms.TextBox tbParcial;
+		private System.Windows.Forms.Timer tmrCronometro;
+	}
+}
+
---
+++ Cronometro/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Cronometro
+{
+	public partial class Form1 : Form
+	{
+		Stopwatch cronometro = new Stopwatch();
+		int vezParcial = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnIniciar_Click(object sender, EventArgs e)
+		{
+			//zerar o cronometro
+			cronometro.Reset();
+
+			//iniciar o cronometro
+			cronometro.Start();
+
+			//zerar as variaveis
+			tbParcial.Text = null;
+			vezParcial = 0;
+
+			//desativar botão iniciar e ativar os de parada
+			btnParcial.Enabled = true;
+			btnParar.Enabled = true;
+			btnIniciar.Enabled = false;
+		}
+
+		private void btnParar_Click(object sender, EventArgs e)
+		{
+			//para o cronômetro
+			cronometro.Stop();
+			
+			//desativar botões de paradas e ativar o de início
+			btnParcial.Enabled = false;
+			btnParar.Enabled = false;
+			btnIniciar.Enabled = true;
+		}
+
+		private void btnParcial_Click(object sender, EventArgs e)
+		{
+			vezParcial++;
+			//jogar parcial no textbox e pular uma linha
+			tbParcial.Text += vezParcial + "- " + lblTempo.Text + Environment.NewLine;
+			//ir para a última linha do textbox
+			tbParcial.SelectionStart = tbParcial.TextLength;
+			tbParcial.ScrollToCaret();
+		}
+
+		private void tmrCronometro_Tick(object sender, EventArgs e)
+		{			
+			lblTempo.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", cronometro.Elapsed.Hours, cronometro.Elapsed.Minutes, cronometro.Elapsed.Seconds, cronometro.Elapsed.Milliseconds/10);
+		}
+	}
+}
